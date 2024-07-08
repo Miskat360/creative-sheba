8f3a9cd6bbff56f3e4e906cd5fb44680c60dbc37
user.name "Miskat Hasan <user.email "miskathasan979@gmail.com>
2024-07-08 18:13:59 +0600

    project setup

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# Creative-Sheba-Design
+## README.md for collaborator
 
-# To run tailwind css paste the command "npx tailwindcss -i ./assets/css/input.css -o ./assets/css/output.css --watch " in your terminal.
+## first --
+**Cloning the Repository**
+
+```bash
+git clone https://github.com/adrianhajdin/portfolio.git
+```
+```bash
+cd portfolio
+```
+
+**Installation**
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+## second -- 
+**[! Very important]**
+**create your won branch.**
+To create a new branch, run these command in your terminal:
+
+```bash
+git switch -C feature/hero-demo
+```
+(note: hero-demo is a demo branch name. You can use any relivent name).
+
+## third --
+**write your code**
+**[! very important]**
+To run tailwind css paste the command in your terminal:
+
+```bash
+npx tailwindcss -i ./assets/css/input.css -o ./assets/css/output.css --watch
+```
+# fourth --
+When you have finished all the work and your project then run these 3 command to your terminal:
+
+```bash
+git add .
+```
+```bash
+git commit -m "your msg"
+```
+```bash
+git push -u origin feature/hero-demo
+```
+(note: Here you must use your branch name. Here 'hero-demo' is my branch name.)
+
+**Font Family**
+- font-primary
+- font-heading
+
+**Color**
+- brown
+- black1
+- white1
+
+(note: you can use this color for any disign "e.g. 'text-brown' to color the text and 'bg-brown' to color the background.")
+
+**Typography fonts**
+use "body-1, body-2, body-3" class for paragraph tag.
+use "h1, h2, h3, h4, h5, h6" for heading tag.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,81 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+// import plugin from "tailwindcss/plugin";
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      heading: "var(--font-heading)",
+      primary: "var(--font-primary)",
+    },
+    screens: {
+      xs: "475px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1400px",
+      "3xl": "1536px",
+    },
+    container: {
+      center: true,
+    },
     extend: {
-      container:{
-        center: true
-      }
+      colors: {
+        brown: "#CDA274",
+        black1: "#292F36",
+        white1: "#F4F0EC",
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [
+    plugin(function ({ addBase, addComponents, addUtilities }) {
+      addBase({});
+      addComponents({
+        ".container": {
+          "@apply max-w-[77.5rem] px-5 md:px-10 lg:px-14 xl:max-w-[82.5rem]":
+            {},
+        },
+        ".h1": {
+          "@apply text-black1 font-heading text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[50px] lg:leading-[4.0625rem] 2xl:text-[3.75rem] 2xl:leading-[4.5rem]":
+            {},
+        },
+        ".h2": {
+          "@apply text-black1 font-heading text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[35px] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight":
+            {},
+        },
+        ".h3": {
+          "@apply text-black1 font-heading text-[25px] leading-normal md:text-[1.9rem]": {},
+        },
+        ".h4": {
+          "@apply text-black1 font-heading text-[22px] leading-normal": {},
+        },
+        ".h5": {
+          "@apply text-black1 font-heading text-xl leading-normal": {},
+        },
+        ".h6": {
+          "@apply text-black1 font-heading text-lg leading-8": {},
+        },
+        ".body-1": {
+          "@apply text-black1 font-primary text-[0.875rem] leading-[1.5rem] md:text-[1rem] md:leading-[1.75rem] lg:text-[1.25rem] lg:leading-8":
+            {},
+        },
+        ".body-2": {
+          "@apply text-black1 font-primary text-base leading-6 md:text-lg": {},
+        },
+        ".body-3": {
+          "@apply text-black1 font-primary font-medium text-[0.875rem] leading-6 md:text-base": {},
+        },
+        ".btn-primary": {
+          "@apply text-white bg-black1 flex items-center gap-2 capitalize tracking-wide text-sm md:text-base font-primary px-5 py-[12px] md:px-8 md:py-[14px] rounded-lg":
+            {},
+        },
+        ".btn-secondary": {
+          "@apply text-white bg-brown flex items-center gap-2 capitalize tracking-wide text-sm md:text-base font-primary px-5 py-[12px] md:px-8 md:py-[14px] rounded-lg":
+            {},
+        },
+      });
+      addUtilities({});
+    }),
+  ],
+};
