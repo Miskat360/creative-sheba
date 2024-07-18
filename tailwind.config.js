d029f812558b94b516d0c70d42ec38d5d3caa1e4
user.name "Miskat Hasan <user.email "miskathasan979@gmail.com>
2024-07-18 11:27:28 +0600

    home page redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,37 +37,37 @@ module.exports = {
             {},
         },
         ".h1": {
-          "@apply text-black1 font-heading400 text-[2rem] xs:text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[50px] lg:leading-[4.0625rem] 2xl:text-[3.75rem] 2xl:leading-[4.5rem]":
+          "@apply text-gray-300 font-heading400 text-[2rem] xs:text-[2.5rem] leading-[3.25rem] md:text-[2.75rem] md:leading-[3.75rem] lg:text-[50px] lg:leading-[4.0625rem] 2xl:text-[3.75rem] 2xl:leading-[4.5rem]":
             {},
         },
         ".h2": {
-          "@apply text-black1 font-heading400 text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[35px] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight":
+          "@apply text-gray-300 font-heading400 text-[1.75rem] leading-[2.5rem] md:text-[2rem] md:leading-[2.5rem] lg:text-[35px] lg:leading-[3.5rem] xl:text-[3rem] xl:leading-tight":
             {},
         },
         ".h3": {
-          "@apply text-black1 font-heading400 text-[25px] leading-normal md:text-[1.9rem]": {},
+          "@apply text-gray-300 font-heading400 text-[25px] leading-normal md:text-[1.9rem]": {},
         },
         ".h4": {
-          "@apply text-black1 font-heading400 text-[22px] leading-normal": {},
+          "@apply text-gray-300 font-heading400 text-[22px] leading-normal": {},
         },
         ".h5": {
-          "@apply text-black1 font-heading400 text-xl leading-normal": {},
+          "@apply text-gray-300 font-heading400 text-xl leading-normal": {},
         },
         ".h6": {
-          "@apply text-black1 font-heading400 text-lg leading-8": {},
+          "@apply text-gray-300 font-heading400 text-lg leading-8": {},
         },
         ".body-1": {
-          "@apply text-black1 font-primary text-[0.875rem] leading-[1.5rem] md:text-[1rem] md:leading-[1.75rem] lg:text-[1.25rem] lg:leading-8":
+          "@apply text-gray-300 font-primary text-[0.875rem] leading-[1.5rem] md:text-[1rem] md:leading-[1.75rem] lg:text-[1.25rem] lg:leading-8":
             {},
         },
         ".body-2": {
-          "@apply text-black1 font-primary text-base leading-6 md:text-lg": {},
+          "@apply text-gray-300 font-primary text-base leading-6 md:text-lg": {},
         },
         ".body-3": {
-          "@apply text-black1 font-primary font-medium text-[0.875rem] leading-6 md:text-base": {},
+          "@apply text-gray-300 font-primary font-medium text-[0.875rem] leading-6 md:text-base": {},
         },
         ".btn-primary": {
-          "@apply text-white bg-black1 flex items-center gap-2 capitalize tracking-wide text-sm md:text-base font-primary px-5 py-[12px] md:px-8 md:py-[14px] rounded-lg hover:bg-black":
+          "@apply text-black bg-zinc-200 flex items-center gap-2 capitalize tracking-wide text-sm md:text-base font-primary px-5 py-[12px] md:px-8 md:py-[14px] rounded-lg hover:bg-white font-semibold":
             {},
         },
         ".btn-secondary": {
